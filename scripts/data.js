f0a61ed691c41c4922cc7d964ee3e19d64859d52
Kang <kmen0004@student.monash.edu>
2019-03-08 00:49:24 +1100

    Site updated: 2019-03-08 00:49:11

---
+++ scripts/data.js
@@ -0,0 +1,207 @@
+var series = {
+    "Flack": {
+        "info": {
+            "imdb": "tt8128344"
+        },
+        "links": {
+            "S01": {
+                "E01": {
+                    "magnet": "magnet:?xt=urn:btih:f902e5eedb42e3b6765cffe471e04c028616ebcb&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E5%85%AC%E5%85%B3.Flack.S01E01.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.HDTVrip.720P-ANON.mp4|521597860|398a670baa48fb36edab2d9e73ff7106|h=tdp6gcx6l6srtlxjev7bgezzrajqsaud|/",
+                    "bd": "https://pan.baidu.com/s/1lwqUGX-1REzNC0cp0GJEvw",
+                    "bdcode": null,
+                    "fantasy": null,
+                    "diyidan": null
+                },
+                "E02": {
+                    "magnet": "magnet:?xt=urn:btih:bb410b88fc83591c959fc8084bca3282c4c341cc&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E5%85%AC%E5%85%B3.Flack.S01E02.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.HDTVrip.720P-ANON.mp4|494980527|0ad4c0ede4dd88f3c9ac0b0e2741d9f6|h=xvgopggvhabigqdamb234aa3qmkjquho|/",
+                    "bd": "https://pan.baidu.com/s/1gHCGoMoQlakv93qUwQKsSw",
+                    "bdcode": "87mq",
+                    "fantasy": null,
+                    "diyidan": null
+                },
+            }
+        }
+    },
+    "Harrow": {
+        "links": {
+            "S01": {
+                "E01": {
+                    "magnet": "magnet:?xt=urn:btih:20bac5435c6420a2fcf72b91fdb8196ad31ef708&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E01.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|620580924|ec2863726e29b037991815f50122c8fd|h=zbt2lv6i7wefj45rsrso4sew7oibqesr|/",
+                    "bd": "https://pan.baidu.com/s/1G9VxKe6kQ4B--yRj9mwkZQ",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1411655/0/146",
+                    "diyidan": null
+                },
+                "E02": {
+                    "magnet": "magnet:?xt=urn:btih:5dbdbd49f4528252c5c151666419edec00ee5581&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E02.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|616602341|e1cd7943a9dea501c09e81a5317d1537|h=akj5xbwnznvuzuxa5xvisj5qqyfkvk4i|/",
+                    "bd": "https://pan.baidu.com/s/1GrWcAw4px_40MHsQrSC0Lw",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1654302/0/146",
+                    "diyidan": null
+                },
+                "E03": {
+                    "magnet": "magnet:?xt=urn:btih:af154fe66cc6dfb979203aba87ea07bf2f578a2f&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E03.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|616523633|297ffd7eb2aeaac20db14383b7007c99|h=a3diet7d53pu3b4nudn3hayr7wkwitiq|/",
+                    "bd": "https://pan.baidu.com/s/12AHYitqBotkBZVgdiWCpdg",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1781669/0/146",
+                    "diyidan": null
+                },
+                "E04": {
+                    "magnet": "magnet:?xt=urn:btih:059b018935bad5d111359847a16f5fa8188901a2&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E04.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|619971483|a8ad125ac0890c2783019bc9ca2d65d5|h=mi3nei4juaznqsetfm72pdngpssrezot|/",
+                    "bd": "https://pan.baidu.com/s/1zZ8P_y3-aWpwqsBHxeHfWw",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1781645/0/146",
+                    "diyidan": null
+                },
+                "E05": {
+                    "magnet": "magnet:?xt=urn:btih:31ae8eddcacb5b67c6501abec00be1fe5930b106&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E05.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.V2.mp4|612157282|a60e720f3ff9d41f6a7fe01ff1a52978|h=pdqpi4ar7ll7d55g6aexfvoqqph5cwe6|/",
+                    "bd": "https://pan.baidu.com/s/1oGw01w4EsoSPYWVaZG8uPA",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1785094/0/146",
+                    "diyidan": null
+                },
+                "E06": {
+                    "magnet": "magnet:?xt=urn:btih:0cd763b858a155cfd226c31f19b232e2bf1de48a&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E06.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|624883471|cc97053d4e13f6704aca1271c223d707|h=m2kus6ducqahbco34p3dmbbfxkjs2rqm|/",
+                    "bd": "https://pan.baidu.com/s/1L-SniyhWJ-iyLMI9dsAy7A",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1866876/0/146",
+                    "diyidan": null
+                },
+                "E07": {
+                    "magnet": "magnet:?xt=urn:btih:cc208b188bfb213de5762c4afa65ef7072f72d44&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E07.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|618792749|113df1e7ebeda9f21ef57b2b23b2fb95|h=ozek5by5kfokfy546yytsx4xq32ffxwl|/",
+                    "bd": "https://pan.baidu.com/s/1stOTLoVDJYfvAVoD8slxWA",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1869000/0/146",
+                    "diyidan": null
+                },
+                "E08": {
+                    "magnet": "magnet:?xt=urn:btih:8fb941a4d257839f9df85c8c7d6b6e9194acc395&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E08.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|618538187|59568f2319612893d7f723a89118575c|h=lfnf6pej2e7holzrc7pu4mibchgkwweg|/",
+                    "bd": "https://pan.baidu.com/s/1zt133Y1jbh1GJcBXpoHbsQ",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1871934/0/146",
+                    "diyidan": null
+                },
+                "E09": {
+                    "magnet": "magnet:?xt=urn:btih:f9375183656ab07c3fa8295cb05ce3eb6e1228d2&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E09.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|624079907|7772ea35f160fe94f59f424e1609d76d|h=3ayjojl44ki2fb6vokh6oj4aexd6nfzh|/",
+                    "bd": "https://pan.baidu.com/s/1ceIWAQ8P_sIE6L16qrQQEA",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1885660/0/146",
+                    "diyidan": null
+                },
+                "E010": {
+                    "magnet": "magnet:?xt=urn:btih:80b33c703814096e75eed627df8fa795fbcd44ee&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|%E7%A5%9E%E7%A7%98%E6%B3%95%E5%8C%BB.Harrow.S01E10.End.%E4%B8%AD%E8%8B%B1%E5%AD%97%E5%B9%95.WEBrip.720P-LOT.mp4|617180474|e79fcf99552f26ab4839f5bf76a08ccc|h=7dfsrrngdykearq36frb2cf7hhtmkcnp|/",
+                    "bd": "https://pan.baidu.com/s/1V7R2BOSb9-ZdRZJ-GL0LYw",
+                    "bdcode": null,
+                    "fantasy": "https://www.onln.cn/video/list/channel/1924777/0/146",
+                    "diyidan": null
+                }
+            }
+        }
+    },
+    "DerryGirls": {
+        "links": {
+            "S01": {
+                "E01": {
+                    "magnet": "magnet:?xt=urn:btih:55071ef2d08a5af1b420f5eb6e8bbd9d592a4faf&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E01.中英字幕.HDTVrip.720P-LOT.mp4|298040122|8d9c8174f0333f7922f8e20167be7fa0|h=oy66urkk2ri4jkcb5m74vvof324f36tf|/",
+                    "bd": "https://pan.baidu.com/s/1jJdoEVk",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/470611/0/146",
+                    "diyidan": ""
+                },
+                "E02": {
+                    "magnet": "magnet:?xt=urn:btih:cd5e8b038171751532707f66b2ec4a3cd184fe5c&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E02.中英字幕.HDTVrip.720P-LOT.mp4|295636353|ac7f366b525560835195154b6e723e35|h=tsjs2mksfuxctne2baocjvsk2vg6h7ps|/",
+                    "bd": "https://pan.baidu.com/s/1dGFtu3v",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/628410/0/146",
+                    "diyidan": ""
+                },
+                "E03": {
+                    "magnet": "magnet:?xt=urn:btih:bc35931e571e375ab248d2d05078bbdd696dc56b&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E03.中英字幕.HDTVrip.720P-LOT.mp4|311152079|df1edaf43125d9ccc69ea7bb439ac8d7|h=j6ksdhdljtgzahzbk2qhyta4iduwtj4s|/",
+                    "bd": "https://pan.baidu.com/s/1o914iIu",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/707897/0/146",
+                    "diyidan": ""
+                },
+                "E04": {
+                    "magnet": "magnet:?xt=urn:btih:74bb25227d8eb66845a8c24b4f08713b36a8d65d&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E04.中英字幕.HDTVrip.720P-LOT.mp4|266486404|8a42ea0c4e732db7db544a12985275d2|h=5q57pciskfo6a2v2kgf5gouaia6mogki|/",
+                    "bd": "https://pan.baidu.com/s/1sngadbv",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/827532/0/146",
+                    "diyidan": ""
+                },
+                "E05": {
+                    "magnet": "magnet:?xt=urn:btih:8c31ae63ed5b5d5dd9020f1c1b3d099cfc87308f&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E05.中英字幕.HDTVrip.720P-LOT.mp4|277209616|c00966f55b79a80426cc9102e2249c88|h=ridp5ouinx7vk4mzb2krpbnu7tcrys3m|/",
+                    "bd": "https://pan.baidu.com/s/1mjylD92",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/1014761/0/146",
+                    "diyidan": ""
+                },
+                "E06": {
+                    "magnet": "magnet:?xt=urn:btih:4228b7cdece9bcc1cdc74533d7acd569241e8f5e&tr=udp://9.rarbg.to:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=http://tr.cili001.com:8070/announce&tr=http://tracker.trackerfix.com:80/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://p4p.arenabg.com:1337&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz",
+                    "ed2k": "ed2k://|file|德里女孩.Derry.Girls.S01E06.End.中英字幕.HDTVrip.720P-LOT.mp4|289798057|1e9aa329ff58573fd9148338d40dacc7|h=sdbpbffk6zypmfmcn2po62p4xukbroop|/",
+                    "bd": "https://pan.baidu.com/s/1i5S3Rnf",
+                    "bdcode": "",
+                    "fantasy": "https://www.onln.cn/video/list/channel/1051833/0/146",
+                    "diyidan": ""
+                },
+            }
+        }
+    },
+    "template": {
+        "links": {
+            "S01": {
+                "E01": {
+                    "magnet": "",
+                    "ed2k": "",
+                    "bd": "",
+                    "bdcode": "",
+                    "fantasy": "",
+                    "diyidan": ""
+                },
+                "E02": {
+                    "magnet": "",
+                    "ed2k": "",
+                    "bd": "",
+                    "bdcode": "",
+                    "fantasy": "",
+                    "diyidan": ""
+                },
+            },
+            "S02": {
+                "E01": {
+                    "magnet": "",
+                    "ed2k": "",
+                    "bd": "",
+                    "bdcode": "",
+                    "fantasy": "",
+                    "diyidan": ""
+                },
+                "E02": {
+                    "magnet": "",
+                    "ed2k": "",
+                    "bd": "",
+                    "bdcode": "",
+                    "fantasy": "",
+                    "diyidan": ""
+                },
+            },
+        }
+    },
+}
---
+++ scripts/process.js
@@ -0,0 +1,38 @@
+function display(name) {
+    list = series[name];
+    $(document).ready(function () {
+        links = list.links;
+        for (season in links) {
+            for (ep in links[season]) {
+                $('#table').append('<tr style="border:0">');
+                $('#table').append('<td style="border:0">' + season + ep + '</td>');
+                if (links[season][ep].magnet) {
+                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].magnet + '">磁力</a></td>');
+                }
+                if (links[season][ep].ed2k) {
+                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].ed2k + '">电驴</a></td>');
+                }
+                if (links[season][ep].bd) {
+                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].bd + '" target="_blank">百度云</a><span>' + (links[season][ep].bdcode?"(提取码：" + links[season][ep].bdcode + ")":"") + "</span>" + '</td>');
+                }
+                if (links[season][ep].fantasy) {
+                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].fantasy + '" target="_blank">范特西</a></td>');
+                }
+                if (links[season][ep].diyidan) {
+                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].diyidan + '" target="_blank">第一弹</a></td>');
+                }
+                $('#table').append('</tr>');
+
+            }
+        }
+        // info = list.info;
+        // $.get("https://www.omdbapi.com/?i=" + info.imdb + "&apikey=b4796ad7", function (data, status) {
+
+        //     $("#score").append(data.imdbRating);
+        //     if (data.imdbRating >= 8) {
+        //         $("#score").css("color", "green");
+        //     }
+
+        // })
+    })
+}
