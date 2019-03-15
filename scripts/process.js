571e887b0d5fabc9aac0d1c483600286f972eff2
anon <lotfansub@sina.com>
2019-03-15 19:01:58 +1100

    Site updated: 2019-03-15 19:01:56

--- scripts/process.js
+++ scripts/process.js
@@ -15,9 +15,9 @@ function display(name) {
                 if (links[season][ep].bd) {
                     $('#table').append('<td style="border:0"><a href="' + links[season][ep].bd + '" target="_blank">百度云</a><span>' + (links[season][ep].bdcode?"(提取码：" + links[season][ep].bdcode + ")":"") + "</span>" + '</td>');
                 }
-                if (links[season][ep].fantasy) {
-                    $('#table').append('<td style="border:0"><a href="' + links[season][ep].fantasy + '" target="_blank">范特西</a></td>');
-                }
+                // if (links[season][ep].fantasy) {
+                //     $('#table').append('<td style="border:0"><a href="' + links[season][ep].fantasy + '" target="_blank">范特西</a></td>');
+                // }
                 if (links[season][ep].diyidan) {
                     $('#table').append('<td style="border:0"><a href="' + links[season][ep].diyidan + '" target="_blank">第一弹</a></td>');
                 }
